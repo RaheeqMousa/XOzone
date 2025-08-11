f5389b81fc617fd4a04de1dd294251e86d60be4c
RaheeqMousa <raheeqmousa00@gmail.com>
2025-08-11 10:24:08 +0300

    Improve/Two Players Experience

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -87,42 +87,65 @@ function makeMove(board, r, c, player) {
     return false;
 }
 
+/*.............This checks if all elements in the array are equal...........................*/
+
+function equalCells(array) {
+    return array.every(cell => cell === array[0] && cell !== 0);
+}
+
 /*...................This function checks if the Game has been finished or yet, if it has been finished it returnes the result...............................*/
 
 function checkGame(board) {
     const n = board.length;
 
-    function equalCells(array) {
-        return array.every(cell => cell === array[0] && cell !== 0);
-    }
-
+    let positions=[];
     // Check rows
     for (let i = 0; i < n; i++) {
-        if (equalCells(board[i])) return board[i][0];
+        
+        if (equalCells(board[i])){
+            positions.push(i);
+            lightUp(positions,n);
+            return board[i][0];
+        }
     }
 
+    positions=[];
     // Check columns
     for (let j = 0; j < n; j++) {
         let col = [];
         for (let i = 0; i < n; i++) {
             col.push(board[i][j]);
+            positions.push(`${i} ${j}`);
         }
-        if (equalCells(col)) return col[0];
+        if (equalCells(col)){
+            lightUp(positions,n);
+            return col[0];
+        } 
     }
 
+    positions=[];
     // Check main diagonal
     let diagonal1 = [];
     for (let i = 0; i < n; i++) {
         diagonal1.push(board[i][i]);
+        positions.push(`${i} ${i}`);
     }
-    if (equalCells(diagonal1)) return diagonal1[0];
+    if (equalCells(diagonal1)){
+        lightUp(positions,n);
+        return diagonal1[0];
+    }
 
+    positions=[];
     // Check anti-diagonal
     let diagonal2 = [];
     for (let i = 0; i < n; i++) {
         diagonal2.push(board[i][n - 1 - i]);
+        positions.push(`${i} ${n - 1 - i}`);
     }
-    if (equalCells(diagonal2)) return diagonal2[0];
+    if (equalCells(diagonal2)){
+        lightUp(positions,n);
+        return diagonal2[0];
+    }
 
     if (board.flat().every(cell => cell !== 0)) return "tie";
 
@@ -132,6 +155,23 @@ function checkGame(board) {
 //Initialize the board on page load
 buildBoard();
 
+/*......................Reset Button...........................*/
+
+const resetBoardBtn=document.querySelector('.ResetBoard');
+if(resetBoardBtn){
+    resetBoardBtn.addEventListener("click",function(){
+        buildBoard();
+        currentPlayer=2;
+
+        const TurnHeading=document.querySelector(".TurnHeading");
+        TurnHeading.classList.remove("LeftRight");
+        void TurnHeading.offsetWidth;
+        TurnHeading.textContent= currentPlayer === 2 ? "Tails's Turn" : "Sonic's Turn";
+        TurnHeading.classList.add("LeftRight");
+
+        document.querySelector('.board').style.pointerEvents='auto'; //enable clicking on the board
+    });
+}
 
 /*..........................Add an event listener to know who clicked and update the board...............................*/
 
@@ -147,6 +187,7 @@ if(board){
         const col = parseInt(event.target.dataset.col);
 
         if (makeMove(gameBoard, row, col, currentPlayer)) {
+            console.log(currentPlayer);
             event.target.textContent = currentPlayer === 1 ? "X" : "O";
 
             const TurnHeading=document.querySelector(".TurnHeading");
@@ -165,7 +206,7 @@ if(board){
                 document.querySelector('.CloseButtonWrapper button').addEventListener("click", () => {
                 document.querySelector(".modal").classList.add("DisplayNone"); });
 
-                buildBoard();
+                TurnHeading.textContent="Sonic is the WINNER!"
             } else if (result === 2) {
                 modal.classList.remove('DisplayNone');
                 winner.innerHTML=`
@@ -175,7 +216,7 @@ if(board){
                 document.querySelector('.CloseButtonWrapper button').addEventListener("click", () => {
                 document.querySelector(".modal").classList.add("DisplayNone"); });
 
-                buildBoard();
+                TurnHeading.textContent="Tails is the WINNER!"
             } else if (result === "tie") {
                 modal.classList.remove('DisplayNone');
                 winner.innerHTML=`
@@ -184,11 +225,29 @@ if(board){
 
                 document.querySelector('.CloseButtonWrapper button').addEventListener("click", () => {
                 document.querySelector(".modal").classList.add("DisplayNone"); });
-                buildBoard();
+                TurnHeading.textContent="It is a tie!"
             } else {
                 currentPlayer = currentPlayer === 1 ? 2 : 1;
                 
             }
         }
     });
+}
+
+/*............................Light up the winning line..................................*/
+
+function lightUp(array,n){
+
+    if(array.length==1){
+        for(let i=0;i<n;i++){
+            document.querySelector(`.cell[data-row="${array[0]}"][data-col="${i}"]`).style.backgroundColor='#04d9ff';
+        }
+    }else{
+        for(let i=0;i<n;i++){
+            let positions=array[i].split(" ");
+            document.querySelector(`.cell[data-row="${positions[0]}"][data-col="${positions[1]}"]`).style.backgroundColor='#04d9ff';
+        }
+    }
+
+    document.querySelector('.board').style.pointerEvents='none'; //disable clicking on the board's divs
 }
